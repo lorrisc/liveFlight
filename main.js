428868a217aa0917706d0e0ce8c7a50af6814b48
Noix2kgouu <l.crappier@gmail.com>
2023-05-13 17:54:11 +0200

    upd flights (10s/drap or move map), day/night phase, marker cluster

--- main.js
+++ main.js
@@ -4,21 +4,49 @@ map.attributionControl.remove();
 
 let googleTerrain = L.tileLayer("http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}", {
     maxZoom: 20,
+    minZoom: 2,
     subdomains: ["mt0", "mt1", "mt2", "mt3"],
 });
 
 googleTerrain.addTo(map);
 
-// coordonnées zone visible carte
-let bounds = map.getBounds();
-let southWest = bounds.getSouthWest();
-let northEast = bounds.getNorthEast();
+// terminator - phase jour/nuit
+var terminator = L.terminator();
+terminator.addTo(map);
 
-// coord max et min
-let latitudeMax = northEast.lat;
-let latitudeMin = southWest.lat;
-let longitudeMax = northEast.lng;
-let longitudeMin = southWest.lng;
+terminator.setStyle({
+    color: "#00",
+    fillColor: "#00",
+    opacity: 0.5,
+});
+
+setInterval(function () {
+    updateTerminator(terminator);
+}, 2000);
+function updateTerminator(terminator) {
+    terminator.setTime();
+}
+
+// limite carte monde
+var southWest_world = L.latLng(-90, -180);
+var northEast_world = L.latLng(90, 180);
+var bounds_world = L.latLngBounds(southWest_world, northEast_world);
+map.setMaxBounds(bounds_world);
+
+function getMapCoord() {
+    // coordonnées zone visible carte
+    let bounds = map.getBounds();
+    let southWest = bounds.getSouthWest();
+    let northEast = bounds.getNorthEast();
+
+    // coord max et min
+    let latitudeMax = northEast.lat;
+    let latitudeMin = southWest.lat;
+    let longitudeMax = northEast.lng;
+    let longitudeMin = southWest.lng;
+
+    return [latitudeMax, latitudeMin, longitudeMax, longitudeMin];
+}
 
 // recupération des vols
 async function getFlights(latMax, latMin, lonMax, lonMin) {
@@ -34,15 +62,6 @@ async function getFlights(latMax, latMin, lonMax, lonMin) {
         });
 }
 
-// Delete all markers
-function clearMarkers() {
-    map.eachLayer((layer) => {
-        if (layer instanceof L.Marker) {
-            map.removeLayer(layer);
-        }
-    });
-}
-
 // marker
 const iconOptions = {
     iconUrl: "assets/plane.svg",
@@ -50,23 +69,57 @@ const iconOptions = {
 };
 const customIcon = L.icon(iconOptions);
 
+var markers = L.markerClusterGroup();
+
 // get and display flights
 function getAndDisplayFlights() {
-    getFlights(latitudeMax, latitudeMin, longitudeMax, longitudeMin).then((data) => {
-        clearMarkers();
+    let coord = getMapCoord();
+    getFlights(coord[0], coord[1], coord[2], coord[3]).then((data) => {
+        markers.clearLayers();
 
-        data.states.forEach((flight) => {
-            // add markers
-            let markerOptions = {
-                icon: customIcon,
-                rotationAngle: flight[10],
-            };
+        console.log("nb vols : " + data.states.length + "  -  heure : " + data.time);
 
-            let markerPoint = new L.marker([flight[6], flight[5]], markerOptions).addTo(map);
+        // display flights
+        if (data.states) {
+            data.states.forEach((flight) => {
+                // add markers
+                let markerOptions = {
+                    icon: customIcon,
+                    rotationAngle: flight[10],
+                };
+
+                markers.addLayer(L.marker([flight[6], flight[5]], markerOptions));
+            });
+            map.addLayer(markers);
+        }
+
+        // update flights on zoom/drag
+        let date = new Date();
+        let isAlreadyUpdate = false;
+
+        map.on("zoom drag", function (event) {
+            if (!isAlreadyUpdate) {
+                isAlreadyUpdate = true;
+
+                let dateZoom = new Date();
+
+                // vérifie le délaie entre les requêtes
+                if (dateZoom - date > 10000) {
+                    getAndDisplayFlights();
+                } else {
+                    setTimeout(() => {
+                        getAndDisplayFlights();
+                    }, 10000 - (dateZoom - date));
+                }
+            }
         });
+
+        // update flights every 10 seconds
+        if (!isAlreadyUpdate) {
+            isAlreadyUpdate = true;
+            setTimeout(getAndDisplayFlights, 10000);
+        }
     });
 }
 
 getAndDisplayFlights();
-
-setInterval(getAndDisplayFlights, 15000);
